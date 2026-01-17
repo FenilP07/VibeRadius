bc72414512a5603ee38a190e1fc888f249cd3ad2
FenilP07 <fenil2755@gmail.com>
2026-01-16 20:36:19 -0500

    implemented an endpoint to get session created by host

--- backend/routes/session.route.js
+++ backend/routes/session.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createSession } from "../controllers/session.controller.js";
+import {
+  createSession,
+  getMySession,
+} from "../controllers/session.controller.js";
 import { isHost } from "../middlewares/host.middleware.js";
 
 const sessionRouter = Router();
 
 sessionRouter.post("/create", isHost, createSession);
+sessionRouter.get("/my", getMySession);
 
 export default sessionRouter;
