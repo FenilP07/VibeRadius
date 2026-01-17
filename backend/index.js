e91ddc94b7f7ae2047704de7ec96da38e77079a5
FenilP07 <fenil2755@gmail.com>
2026-01-17 12:37:12 -0500

    initialized socket io in backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
+import http from "http";
 import connectDB from "./configs/db.config.js";
 import logger from "./utils/logger.js";
 import { app } from "./app.js";
+import { Server } from "socket.io";
 
 const PORT = process.env.PORT || 5000;
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  logger.info(`Socket connected: ${socket.id}`);
+});
+
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       logger.info(`server is running on port ${PORT}`);
     });
   })
