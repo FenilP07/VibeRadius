0eaaa005b59fa8e5668cfb699836846d54d672f5
FenilP07 <fenil2755@gmail.com>
2026-01-16 20:36:19 -0500

    implemented an endpoint to get session created by host

--- backend/controllers/session.controller.js
+++ backend/controllers/session.controller.js
@@ -4,43 +4,65 @@
   import logger from "../utils/logger.js";
   import { APIResponse } from "../utils/ApiResponse.js";
 
+//create session
+const createSession = asyncHandler(async (req, res) => {
+  const { sessionName } = req.body;
+  const hostId = req.user._id;
+  const code = generateSessionCode();
 
-  const createSession = asyncHandler(async (req, res) => {
-    const { sessionName } = req.body;
-    const hostId = req.user._id;
-    const code = generateSessionCode();
-
-    logger.info("Create session request received", {
-      hostId,
-      sessionName,
-    });
-
-    const session = await Session.create({
-      host_id: hostId,
-      session_name: sessionName,
-      session_code: code,
-    });
-
-    logger.info("Session created successfully", {
-      sessionId: session._id,
-      sessionCode: session.session_code,
-      hostId,
-    });
-
-    return res.status(201).json(
-      new APIResponse(
-        201,
-        {
-          session: {
-            _id: session._id,
-            host_id: session.host_id,
-            session_name: session.session_name,
-            session_code: session.session_code,
-          },
-        },
-        "Session created succesfully"
-      )
-    );
+  logger.info("Create session request received", {
+    hostId,
+    sessionName,
   });
 
-  export { createSession };
+  const session = await Session.create({
+    host_id: hostId,
+    session_name: sessionName,
+    session_code: code,
+  });
+
+  logger.info("Session created successfully", {
+    sessionId: session._id,
+    sessionCode: session.session_code,
+    hostId,
+  });
+
+  return res.status(201).json(
+    new APIResponse(
+      201,
+      {
+        session: {
+          _id: session._id,
+          host_id: session.host_id,
+          session_name: session.session_name,
+          session_code: session.session_code,
+        },
+      },
+      "Session created succesfully"
+    )
+  );
+});
+
+//get session by hostID
+
+const getMySession = asyncHandler(async (req, res) => {
+  const hostId = req.user._id;
+
+  const session = await Session.find({ host_id: hostId }).sort({
+    createdAt: -1,
+  });
+
+  return res
+    .status(200)
+    .json(new APIResponse(201, { session }, "session fetched succesfully"));
+});
+
+//join session
+
+//leave session
+
+//deleate sesssion
+
+//session status cahange
+
+export { createSession, getMySession };
