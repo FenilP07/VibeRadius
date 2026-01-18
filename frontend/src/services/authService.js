00d9e3a3074736666cefc100f2e2acd86346719f
henil_codes <c0940069@mylambton.ca>
2026-01-18 16:41:48 -0500

    resolved dependencies error

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from 'axios' 
 
-const API_URL = import.meta.env.VITE_API_BASE_URL
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 const authApiPrefix = '/api/auth'
 
 // Create axios instance
