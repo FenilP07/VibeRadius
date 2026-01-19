045531ddafecd448b8b43338f1c38f989f1995c2
FenilP07 <fenil2755@gmail.com>
2026-01-18 23:23:23 -0500

    root file fix  spotify call back should work now

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,29 +1,25 @@
-import { Route, Routes } from 'react-router-dom'
-import StyleGuide from '../StyleGuide'
-import { AdminDashboard } from './pages/admin/AdminDashboard'
-import SpotifySearch from './components/SpotifySearch'
-import Login from './pages/auth/Login.jsx'
-import Register from './pages/auth/Register.jsx'
-
+import { Route, Routes } from "react-router-dom";
+import StyleGuide from "../StyleGuide";
+import { AdminDashboard } from "./pages/admin/AdminDashboard";
+import SpotifySearch from "./components/SpotifySearch";
+import Login from "./pages/auth/Login.jsx";
+import Register from "./pages/auth/Register.jsx";
+import HomePage from "./pages/homePage.jsx";
 
 export default function AppRoutes() {
-    return(
-        <Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
 
-            {/* For Home
-            <Route path='/' element={<SpotifySearch/>}></Route> */}
+      {/* For Style Guide */}
+      <Route path="/styleguide" element={<StyleGuide />} />
 
-            {/* For Style Guide */}
-            <Route path='/styleguide' element={<StyleGuide/>} />
+      {/* For Admin Dashboard */}
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
 
-            {/* For Admin Dashboard */}
-            <Route path='/' element={<AdminDashboard/>} />
-
-            {/* For Authentication */}
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/register' element={<Register/>}></Route>
-
-        </Routes>
-    )
+      {/* For Authentication */}
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+    </Routes>
+  );
 }
-
